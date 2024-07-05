56d986f84a6b50d3708ea71aae597f02bf79b2f9
SARANYA G <167203620+Saranya091003@users.noreply.github.com>
2024-07-05 11:59:14 +0530

    Add files via upload

---
+++ hb-inheritance-examples/src/main/java/com/model/RegEmployee.java
@@ -0,0 +1,31 @@
+package com.model;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("RegEmployee")
+public class RegEmployee extends Employee{
+	float salary;
+	String dept;
+	public RegEmployee(float salary, String dept, String name) {
+		super(name);
+		this.salary = salary;
+		this.dept = dept;
+	}
+	public float getSalary() {
+		return salary;
+	}
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	
+	
+
+}
---
+++ hb-inheritance-examples/src/main/java/com/model/Trainee.java
@@ -0,0 +1,30 @@
+package com.model;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("Trainee")
+public class Trainee extends Employee{
+	float payperhour;
+	String contractperiod;
+	public Trainee(float payperhour, String contractperiod, String name) {
+		super(name);
+		this.payperhour = payperhour;
+		this.contractperiod = contractperiod;
+	}
+	public float getPayperhour() {
+		return payperhour;
+	}
+	public void setPayperhour(float payperhour) {
+		this.payperhour = payperhour;
+	}
+	public String getContractperiod() {
+		return contractperiod;
+	}
+	public void setContractperiod(String contractperiod) {
+		this.contractperiod = contractperiod;
+	}
+	
+
+}
